d76cf8710d24bba0751e3cfcde1ae4b65d565e20
chongkessob <chonks091004@gmail.com>
2025-03-23 20:14:26 -0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -54,7 +54,7 @@ PRODUCT_PACKAGES_DEBUG += \
  
 # Fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.1-impl-mock \
     fastbootd
 
 PRODUCT_PACKAGES += \
